bugfix data not defined when not masking. Started on multi frequency analysis

HaloFitting.py
@@ -97,8 +97,8 @@ def get_initial_guess(halo):
     diff   = np.abs(halo.margin)
     p0     = [
         halo.I0, 
-        Halo.centre_pix[0]-Halo.fov_info_mcmc[2], 
-        Halo.centre_pix[1]-Halo.fov_info_mcmc[0], 
+        halo.centre_pix[0]-halo.fov_info_mcmc[2], 
+        halo.centre_pix[1]-halo.fov_info_mcmc[0], 
         r_guess,r_guess,r_guess,r_guess,0.,0.,0.
     ]
     bounds = ([0.,0.,0.,0.,0.,0.,0.,-np.inf, 0., -np.inf],
@@ -159,5 +159,5 @@ if __name__=='__main__':
     processing.get_flux(int_max=args.int_max, freq=frequency)# error is one sigma (68%).
     processing.get_power(freq=frequency)
 
-    print(processing.popt_units)
+    print(processing)
     #Halo.Close()

halo_fdca/fdca_utils.py
@@ -37,11 +37,19 @@ def get_initial_guess(halo):
     if r_guess >= halo.data.shape[1]/2.: r_guess = halo.data.shape[1]/4.
 
     diff   = np.abs(halo.margin)
-    p0     = [halo.I0, halo.centre_pix[0]+diff[0],
-              halo.centre_pix[1]+diff[2], r_guess,r_guess,r_guess,r_guess,0.,0.,0.]
+    p0     = [
+        halo.I0, 
+        halo.centre_pix[0]+halo.margin[2], 
+        halo.centre_pix[1]+halo.margin[0], 
+        r_guess,r_guess,r_guess,r_guess,0.,0.,0.
+    ]
     bounds = ([0.,0.,0.,0.,0.,0.,0.,-np.inf, 0., -np.inf],
               [np.inf,halo.data.shape[0],halo.data.shape[1],
                r_bound,r_bound,r_bound,r_bound,np.inf, np.inf, np.inf])
+    print("initial guess")
+    print(halo.centre_pix, halo.margin)
+    print(p0)
+    print()
     return p0,bounds
 
 def add_parameter_labels(obj, array) -> pd.Series|pd.DataFrame:
@@ -294,6 +302,18 @@ def regridding(obj, data, decrease_fov=False, mask=False):
     regrid   = regrid_to_beamsize(obj, data_rot)*data.unit
     return regrid
 
+def pad_image(img):
+    pivot = ((np.sqrt(2.0) / 2.0 - 0.5) * np.array(img.shape)).astype(
+        np.int64
+    )
+    padX = [pivot[0], pivot[0]]
+    padY = [pivot[1], pivot[1]]
+    padded_img = np.pad(img, [padY, padX], "constant")
+    fov_info = [
+        -pivot[0],img.shape[0] + pivot[0],-pivot[1],img.shape[1] + pivot[1]
+    ]
+    return padded_img, fov_info
+
 def rotate_image(obj,img, decrease_fov=False, mask=False):
     if mask: cval=1
     else: cval=0
@@ -353,23 +373,23 @@ def gamma_dist(x, shape, scale):
     return (x**(shape-1.)*np.exp(-x/scale))/(gamma(shape)*(scale**shape))
 
 
-def transform_units(obj, params):
-    params[1] = params[1] + obj.halo.fov_info_mcmc[2]
-    params[2] = params[2] + obj.halo.fov_info_mcmc[0]
-    param_sky = wcs.utils.pixel_to_skycoord(
-        params[1], params[2], wcs.WCS(obj.halo.header), origin=1
-    )
-    params[1] = param_sky.ra.deg
-    params[2] = param_sky.dec.deg
+def transform_units(obj, params, err=False):
+    if err:
+        params[1] *= obj.halo.pix_size.to(u.deg).value
+        params[2] *= obj.halo.pix_size.to(u.deg).value
+    else:
+        params[1] += obj.halo.fov_info_mcmc[2]
+        params[2] += obj.halo.fov_info_mcmc[0]
+        param_sky = wcs.utils.pixel_to_skycoord(
+            params[1], params[2], wcs.WCS(obj.halo.header), origin=1
+        )
+        params[1] = param_sky.ra.deg
+        params[2] = param_sky.dec.deg
     
     params[0] = ((u.Jy * params[0] / obj.halo.pix_area).to(uJyarcsec2)).value
-    #params[1] = (
-    #    params[1] - self.centre_pix[0]
-    #) * self.halo.pix_size.value + self.centre_wcs.ra.deg
-    #params[2] = (
-    #    params[2] - self.centre_pix[1]
-    #) * self.halo.pix_size.value + self.centre_wcs.dec.deg
+    
     params[3] = ((params[3] * obj.halo.pix2kpc).to(u.kpc)).value
+    
     if obj.modelName in ["ellipse", "rotated_ellipse", "skewed"]:
         params[4] = ((params[4] * obj.halo.pix2kpc).to(u.kpc)).value
 

halo_fdca/halo_object.py
@@ -164,7 +164,6 @@ class RadioHalo(object):
         self.name = self.target.replace("Abell", "Abell ")
         self.name = self.target.replace("WHL", "")
         
-        
 
         self.initiatePaths(mask_path, output_path)
         data, hader = self.load_data()
@@ -175,14 +174,6 @@ class RadioHalo(object):
         self.get_object_location(loc)
         self.extract_object_info(M500, R500, z)
         
-        x = np.arange(0, data.shape[1], step=1, dtype="float")
-        y = np.arange(0, data.shape[0], step=1, dtype="float")
-        self.x_pix, self.y_pix = np.meshgrid(x, y)
-
-        self.fov_info = [0, data.shape[0], 0, data.shape[1]]
-        self.image_mask, self.mask = utils.masking(self, mask)
-        self.exponentialFit(data, first=True)  # Find centre of the image centre_pix
-
         if self.header["BUNIT"] == "JY/BEAM" or self.header["BUNIT"] == "Jy/beam":
             self.data = data * (u.Jy / self.beam2pix)
         else:
@@ -191,6 +182,16 @@ class RadioHalo(object):
                 "Possibly other units than jy/beam, CHECK HEADER UNITS!",
             )
             sys.exit()
+        
+        x = np.arange(0, data.shape[1], step=1, dtype="float")
+        y = np.arange(0, data.shape[0], step=1, dtype="float")
+        self.x_pix, self.y_pix = np.meshgrid(x, y)
+
+        self.fov_info = [0, data.shape[0], 0, data.shape[1]]
+        self.image_mask, self.mask = utils.masking(self, mask)
+        self.exponentialFit(data, first=True)  # Find centre of the image centre_pix
+
+        
 
         self.pix_to_world()
         self.set_image_characteristics(decreased_fov)
@@ -357,18 +358,8 @@ class RadioHalo(object):
             self.image_mask, self.mask = utils.masking(self, self.mask)
             self.exponentialFit(self.data.value)
         else:
-            pivot = ((np.sqrt(2.0) / 2.0 - 0.5) * np.array(self.data.shape)).astype(
-                np.int64
-            )
-            padX = [pivot[0], pivot[0]]
-            padY = [pivot[1], pivot[1]]
-            self.data_mcmc = np.pad(self.data, [padY, padX], "constant")
-            self.fov_info_mcmc = [
-                -pivot[0],
-                self.data.shape[0] + pivot[0],
-                -pivot[1],
-                self.data.shape[1] + pivot[1],
-            ]
+            self.data_mcmc, self.fov_info_mcmc = utils.pad_image(self.data)
+            
             self.fov_info = [0, self.data.shape[0], 0, self.data.shape[1]]
             self.margin = np.array(self.fov_info) - np.array(self.fov_info_mcmc)
             self.data = self.data[
@@ -528,7 +519,10 @@ class RadioHalo(object):
             size = data.shape[1] / 4.0
             max_flux = np.max(plotdata)
         else:
-            centre_pix = self.centre_pix
+            if self.cropped:
+                centre_pix = [self.centre_pix[0] - self.fov_info[2], self.centre_pix[1] - self.fov_info[0]]
+            else:
+                centre_pix = self.centre_pix
             size = self.radius / (3.5 * self.pix_size)
             max_flux = self.I0
             

halo_fdca/markov_chain_monte_carlo.py
@@ -120,6 +120,7 @@ class Fitting(object):
         if bounds is None: self.bounds = bounds_temp
         else: self.bounds = bounds
         
+        mask = _parent_.mask
         self.orig_shape = _parent_.data.shape
         self.rebin = rebin
         self.halo = _parent_
@@ -141,15 +142,11 @@ class Fitting(object):
         return
 
     def pre_fit(self):
-        # try:
-        popt = self.pre_mcmc_fit(
+        popt, perr = self.pre_mcmc_fit(
             self.data, p0=np.array(self.p0), bounds=np.array(self.bounds)
         )
-        return popt
-        # except Exception as e:
-        #    self.log.log(logging.CRITICAL,'MCMC Failed to execute pre-fit with error message:\n')
-        #    self.log.log(logging.CRITICAL,e)
-        #    sys.exit()
+        return popt, perr
+
 
     def run(self, pre_fit_guess=None, save=False):
         data = self.set_data_to_use(self.data)
@@ -158,7 +155,7 @@ class Fitting(object):
         coord = np.meshgrid(x, y)
         
         if pre_fit_guess is None:
-            self.popt = self.pre_fit()
+            self.popt, __ = self.pre_fit()
         else:
             self.popt = pre_fit_guess
             
@@ -303,10 +300,14 @@ class Fitting(object):
             )
 
         self.image_mask, self.mask = utils.masking(self, mask, full_size=True)
-        self.image_mask = self.image_mask[
-            self.halo.fov_info_mcmc[0] : self.halo.fov_info_mcmc[1],
-            self.halo.fov_info_mcmc[2] : self.halo.fov_info_mcmc[3],
-        ]
+        
+        if np.any(np.asarray(self.halo.fov_info_mcmc) < 0):
+            self.image_mask, fov_info = utils.pad_image(self.image_mask)
+        else:
+            self.image_mask = self.image_mask[
+                self.halo.fov_info_mcmc[0] : self.halo.fov_info_mcmc[1],
+                self.halo.fov_info_mcmc[2] : self.halo.fov_info_mcmc[3],
+            ]
 
         if burntime is None:
             self.burntime = int(0.125 * self.steps)
@@ -412,6 +413,7 @@ class Fitting(object):
         #popt[1] += self.halo.margin[2]
         #popt[2] += self.halo.margin[0]
         popt = utils.add_parameter_labels(self, popt)
+        perr = utils.add_parameter_labels(self, perr)
 
         if not self.k_exponent:
             popt["k_exp"] = 0.5
@@ -460,7 +462,7 @@ class Fitting(object):
         y = np.arange(0, self.data.shape[0], 1)
         self.x_pix, self.y_pix = np.meshgrid(x, y)
         
-        return popt
+        return popt, perr
 
     def plotSampler(self):
         fig, axes = plt.subplots(ncols=1, nrows=self.dim, sharex=True)
@@ -865,6 +867,7 @@ class Processing(object):
         self.k_exponent = k_exponent
         self.offset = offset
         self.mask_treshold = 0.5
+        mask = _parent_.mask
 
         self.check_settings(model, mask)
         self.extract_chain_file(rebin)
@@ -872,6 +875,37 @@ class Processing(object):
         self.set_labels_and_units()
 
         self.dof = len(self.data.value.flat) - self.dim
+        
+    def __repr__(self) -> str:
+        
+        uncertainties1 = self.percentiles_units[:, 1] - self.percentiles_units[:, 0]
+        uncertainties2 = self.percentiles_units[:, 2] - self.percentiles_units[:, 1]
+
+
+        param_string = ""
+        for param in range(len(self.params[self.params])):
+            param_string += f"{self.paramNames[param]}:   {self.params_units[param]:.5f} ({self.units[param]})\n    "
+        
+        run_details = f"""
+Run information for object {self.halo.name}:
+    RMS noise: {self.rms}
+    Model: {self.modelName}
+    Walkers: {self.walkers}
+    Steps: {self.steps}
+    Burntime: {self.burntime}
+    Mask: {self.mask}
+    Rebin: {self.rebin}
+    K_exponent: {self.k_exponent}
+    Offset: {self.offset}
+
+Fit results:
+    Reduced chi-squared: {self.get_chi2_value()}
+    {param_string}
+    Uncertainties (lower, upper):
+        {uncertainties1}
+        {uncertainties2}
+    """
+        return run_details
 
     def plot(self):
         plot_fits.fit_result(
@@ -1186,17 +1220,22 @@ class Processing(object):
         self.get_units()
         uncertainties1 = self.percentiles_units[:, 1] - self.percentiles_units[:, 0]
         uncertainties2 = self.percentiles_units[:, 2] - self.percentiles_units[:, 1]
-        self.log.log(
-            logging.INFO,
-            "\n Parameters: \n%s \nOne sigma parameter uncertainties (lower, upper): \
-                                    \n%s \n%s \nIn Units: %s"
-            % (
+        '''
+        string_to_print = "\n Parameters: \n%s \nOne sigma parameter uncertainties (lower, upper): \
+                                    \n%s \n%s \nIn Units: %s" % (
                 str(self.params_units[self.params]),
                 str(uncertainties1),
                 str(uncertainties2),
                 str(self.units),
-            ),
-        )
+            )
+        '''                         
+        string_to_print = f"\n Parameters: \n{str(self.params_units[self.params])} \
+            \nOne sigma parameter uncertainties (lower, upper): \
+            \n{str(uncertainties1)} \n{str(uncertainties2)} \
+            \nIn Units: {str(self.units)}" 
+                                    
+        self.log.log(logging.INFO, string_to_print)
+        return string_to_print
 
     def get_units(self):
         labels = ["$I_0$", "$x_0$", "$y_0$"]
@@ -1345,6 +1384,7 @@ class Processing(object):
         x = np.arange(0, self.data.shape[1], 1)
         y = np.arange(0, self.data.shape[0], 1)
         self.x_pix, self.y_pix = np.meshgrid(x, y)
+        return self.chi2_red
 
     def get_flux(self, int_max=np.inf, freq=None):
         if freq is None: