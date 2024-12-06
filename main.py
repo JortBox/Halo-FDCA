0d1f54767f503c3793e6e43fee3db7533e00ec87
Jort Box <jboxelaar98@outlook.com>
2024-12-06 23:26:25 +0100

    update

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import halo_fdca as fdca
 
 def regular_run():
     Halo = fdca.RadioHalo(
-        'A2390', 
-        'Example/data_dir_test/A2390_60asec_sub1-MFS-image.fits', 
+        'A2744', 
+        'Example/Data_dir/A2744_JVLA.image.fits', 
         output_path='Example/Output_dir', 
-        z=0.2269, 
-        #mask_path='Example/Data_dir/A2744halo.reg',
+        z=0.308, 
+        mask_path='Example/Data_dir/A2744halo.reg',
         decreased_fov=True
     )
     
@@ -21,15 +21,15 @@ def regular_run():
         "y0": 315.,
     }
 
-    fit = fdca.Fit(Halo, model=['rotated_ellipse','rotated_ellipse'], link_loc=[True, True], profiles=["default", "gaussian"], steps=500, walkers=100)
-    #fit = fdca.Fit(Halo, walkers=10, steps=100, model='rotated_ellipse')#, freeze_params=freeze)
-    #fit.run()
-    #fit.save()
+    fit = fdca.Fit(Halo, walkers=10, steps=100, model='rotated_ellipse')#, freeze_params=freeze)
+    fit.run()
+    fit.save()
     fit.load()
     
-    #results = fdca.Processing(fit)
-    results = fdca.ProcessingMulticomponent(fit)
-    results.plot()
+    chi2 = fit.results.get_chi2()
+    flux = fit.results.get_flux()
+    fit.results.plot()
+    
     
 
 def load_from_file():
